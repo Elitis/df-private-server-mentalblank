e8d9a47d139df603ebda57bdc43145f2be3a26ae
blaaarrrggghhh <blaaarrrggghhh@gmail.com>
2012-02-20 01:26:52 +0000

    Fixed SQL Syntax Error that did not allow signup

--- cf-usersignup.php
+++ cf-usersignup.php
@@ -13,9 +13,15 @@ $password = md5(sha1($_POST['strPassword'], $salt));
 
 $email = mysql_real_escape_string(stripslashes($_POST['strEmail']));
 $dob = mysql_real_escape_string(stripslashes($_POST['strDOB']));
-$user_result = mysql_query("SELECT * FROM df_users WHERE name = '" . $username . "'") or die(mysql_error());
+$user_result = mysql_query("SELECT * FROM df_users WHERE name = '".$username."'") or $error = true;
 
-if($username == "" || $password == "" || $email == "" || $dob == ""){
+if($error == true){
+	$status = "Failure";
+    $message = "Database Issue.";
+    $reason = "Unable to communicate with database properly";
+    $errorMessage = "No error.";
+	$error = true;
+} else if($username == "" || $password == "" || $email == "" || $dob == ""){
        	$status = "Failure";
        	$message = "Something went Wrong.";
        	$reason = "Error";
@@ -23,12 +29,19 @@ if($username == "" || $password == "" || $email == "" || $dob == ""){
        	$error = true;
 } else {
 	if (mysql_num_rows($user_result) == 0) {
-		$user_insert = mysql_query("INSERT INTO df_users (name, pass, salt, email, dob) VALUES ('$username', '$password', '$salt', $email', '$dob')") or die("status=Failure&strReason=" . mysql_error());
-		$user_result = mysql_query("SELECT * FROM df_users WHERE name = '" . $username . "'") or die(mysql_error());
+		$user_insert = mysql_query("INSERT INTO df_users (name, pass, salt, email, dob) VALUES ('".$username."', '".$password."', '".$salt."', '".$email."', '".$dob."')") or $error = true;
+		$user_result = mysql_query("SELECT * FROM df_users WHERE name = '".$username."'") or $error = true;
 		$user = mysql_fetch_assoc($user_result);
+		if($error == true){
+			$status = "Failure";
+       		$message = "Database Issue.";
+       		$reason = "Could not add user to database";
+       		$errorMessage = "No error.";
+		} else {
        		echo("&ID=" . $user['id']);
-		$status = "Success";
-		$message = "Account Created Successfully";
+			$status = "Success";
+			$message = "Account Created Successfully";
+		}
 	} else {
        		$status = "Failure";
        		$message = "Your username is already taken by another user account.";
@@ -44,4 +57,6 @@ if ($error) {
 	echo("&strReason=" . $reason);
 	echo("&strErr=" . $errorMessage);
 }
+
+echo("&strButtonName=Continue");
 ?>
